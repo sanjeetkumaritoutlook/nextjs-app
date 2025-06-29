edd3f55f8de530de7dbd19cefd9fc0ba729205df
sanjeetkumaritoutlook-user <sanjeetkumarit@outlook.com>
2025-06-29 06:13:17 +0530

    using third party library in nextjs react 19

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,28 @@
+'use client';
+
 import ContactForm from '../ContactForm'; // Import your form component
+import { MyButton, MyCard } from 'react19-cra'
+
 
 const ContactPage = () => {
   return (
     <div>
       <h1>Contact Us</h1>
       <ContactForm /> {/* Use the ContactForm here */}
+        <div style={{ padding: 20 }}>
+      <MyCard
+        title="Hello Card"
+        description="This card is from my custom library."
+        image="https://picsum.photos/300/150"
+        onClick={() => alert('Card clicked!')}
+      >
+        <MyButton
+          label="Click Me"
+          variant="primary"
+          onClick={() => alert('Button clicked!')}
+        />
+      </MyCard>
+    </div>
     </div>
   );
 };
